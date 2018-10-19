c9e0ae5cb6f94a9d9476fc4c47c253972b20a6f4
Leon Le <lle4@ggc.edu>
2018-10-19 10:20:23 -0400

    adding sunny's code

--- WindowsFormsApp1/WindowsFormsApp1/MainMenu.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainMenu.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            new LevelSelect().Show();
+            new level1().Show();
             Hide();
         }
 
@@ -27,6 +27,21 @@ namespace WindowsFormsApp1
         {
             new LevelSelect().Show();
             Hide();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)//restartbutton
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
