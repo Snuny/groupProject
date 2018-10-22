2605b4e02253f153969573e97534bf4eb727da9d
Leon Le <lle4@ggc.edu>
2018-10-22 10:34:37 -0400

    Level 1 button now goes to level 1

--- WindowsFormsApp1/WindowsFormsApp1/LevelSelect.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LevelSelect.Designer.cs
@@ -28,87 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.level1Button = new System.Windows.Forms.Button();
+            this.userlevelButton = new System.Windows.Forms.Button();
+            this.level5Button = new System.Windows.Forms.Button();
+            this.level4Button = new System.Windows.Forms.Button();
+            this.level3Button = new System.Windows.Forms.Button();
+            this.level2Button = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // level1Button
             // 
-            this.button1.BackColor = System.Drawing.Color.Gold;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(93, 114);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 51);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Level 1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.level1Button.BackColor = System.Drawing.Color.Gold;
+            this.level1Button.FlatAppearance.BorderSize = 0;
+            this.level1Button.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level1Button.Location = new System.Drawing.Point(93, 114);
+            this.level1Button.Name = "level1Button";
+            this.level1Button.Size = new System.Drawing.Size(145, 51);
+            this.level1Button.TabIndex = 0;
+            this.level1Button.Text = "Level 1";
+            this.level1Button.UseVisualStyleBackColor = false;
+            this.level1Button.Click += new System.EventHandler(this.level1Button_Click);
             // 
-            // button2
+            // userlevelButton
             // 
-            this.button2.BackColor = System.Drawing.Color.Gold;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(550, 269);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 51);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "User Levels";
-            this.button2.UseVisualStyleBackColor = false;
+            this.userlevelButton.BackColor = System.Drawing.Color.Gold;
+            this.userlevelButton.FlatAppearance.BorderSize = 0;
+            this.userlevelButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userlevelButton.Location = new System.Drawing.Point(550, 269);
+            this.userlevelButton.Name = "userlevelButton";
+            this.userlevelButton.Size = new System.Drawing.Size(145, 51);
+            this.userlevelButton.TabIndex = 1;
+            this.userlevelButton.Text = "User Levels";
+            this.userlevelButton.UseVisualStyleBackColor = false;
+            this.userlevelButton.Click += new System.EventHandler(this.userlevelButton_Click);
             // 
-            // button3
+            // level5Button
             // 
-            this.button3.BackColor = System.Drawing.Color.Gold;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(323, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 51);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Level 5";
-            this.button3.UseVisualStyleBackColor = false;
+            this.level5Button.BackColor = System.Drawing.Color.Gold;
+            this.level5Button.FlatAppearance.BorderSize = 0;
+            this.level5Button.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level5Button.Location = new System.Drawing.Point(323, 269);
+            this.level5Button.Name = "level5Button";
+            this.level5Button.Size = new System.Drawing.Size(145, 51);
+            this.level5Button.TabIndex = 2;
+            this.level5Button.Text = "Level 5";
+            this.level5Button.UseVisualStyleBackColor = false;
+            this.level5Button.Click += new System.EventHandler(this.level5Button_Click);
             // 
-            // button4
+            // level4Button
             // 
-            this.button4.BackColor = System.Drawing.Color.Gold;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(93, 269);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(145, 51);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Level 4";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.level4Button.BackColor = System.Drawing.Color.Gold;
+            this.level4Button.FlatAppearance.BorderSize = 0;
+            this.level4Button.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level4Button.Location = new System.Drawing.Point(93, 269);
+            this.level4Button.Name = "level4Button";
+            this.level4Button.Size = new System.Drawing.Size(145, 51);
+            this.level4Button.TabIndex = 3;
+            this.level4Button.Text = "Level 4";
+            this.level4Button.UseVisualStyleBackColor = false;
+            this.level4Button.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // level3Button
             // 
-            this.button5.BackColor = System.Drawing.Color.Gold;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(550, 114);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(145, 51);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Level 3";
-            this.button5.UseVisualStyleBackColor = false;
+            this.level3Button.BackColor = System.Drawing.Color.Gold;
+            this.level3Button.FlatAppearance.BorderSize = 0;
+            this.level3Button.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level3Button.Location = new System.Drawing.Point(550, 114);
+            this.level3Button.Name = "level3Button";
+            this.level3Button.Size = new System.Drawing.Size(145, 51);
+            this.level3Button.TabIndex = 4;
+            this.level3Button.Text = "Level 3";
+            this.level3Button.UseVisualStyleBackColor = false;
+            this.level3Button.Click += new System.EventHandler(this.level3Button_Click);
             // 
-            // button6
+            // level2Button
             // 
-            this.button6.BackColor = System.Drawing.Color.Gold;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(323, 114);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(145, 51);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Level 2";
-            this.button6.UseVisualStyleBackColor = false;
+            this.level2Button.BackColor = System.Drawing.Color.Gold;
+            this.level2Button.FlatAppearance.BorderSize = 0;
+            this.level2Button.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level2Button.Location = new System.Drawing.Point(323, 114);
+            this.level2Button.Name = "level2Button";
+            this.level2Button.Size = new System.Drawing.Size(145, 51);
+            this.level2Button.TabIndex = 5;
+            this.level2Button.Text = "Level 2";
+            this.level2Button.UseVisualStyleBackColor = false;
+            this.level2Button.Click += new System.EventHandler(this.level2Button_Click);
             // 
             // button7
             // 
@@ -130,12 +135,12 @@
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.level2Button);
+            this.Controls.Add(this.level3Button);
+            this.Controls.Add(this.level4Button);
+            this.Controls.Add(this.level5Button);
+            this.Controls.Add(this.userlevelButton);
+            this.Controls.Add(this.level1Button);
             this.Name = "LevelSelect";
             this.Text = "LevelSelect";
             this.ResumeLayout(false);
@@ -144,12 +149,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button level1Button;
+        private System.Windows.Forms.Button userlevelButton;
+        private System.Windows.Forms.Button level5Button;
+        private System.Windows.Forms.Button level4Button;
+        private System.Windows.Forms.Button level3Button;
+        private System.Windows.Forms.Button level2Button;
         private System.Windows.Forms.Button button7;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/LevelSelect.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LevelSelect.cs
@@ -17,15 +17,42 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void button7_Click(object sender, EventArgs e)
         {
             new MainMenu().Show();
             Hide();
+        }
+
+        private void level1Button_Click(object sender, EventArgs e)
+        {
+            new FallingRocks.level1().Show();
+            Hide();
+        }
+
+        private void level2Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void level3Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //level4Button
+        {
+
+        }
+
+        private void level5Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userlevelButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
